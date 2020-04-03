6b5675c3cb0b4067c4b878e19ce12eee9f3a34de
bluefireoly <bluefireolymp@gmail.com>
2020-04-03 14:51:45 +0200

    Update MariaDB.java
    - removed the retry parameter
    - validate and isConnected functions are now static
    - removed unnessecary try-catch blocks
    - executePreparedStatement now throws SQLException
    - preparedStatement now gets closed

--- src/main/java/net/axay/blueutils/sql/MariaDB.java
+++ src/main/java/net/axay/blueutils/sql/MariaDB.java
@@ -1,14 +1,15 @@
 package net.axay.blueutils.sql;
 
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 import org.mariadb.jdbc.MariaDbPoolDataSource;
 
+import java.io.Closeable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class MariaDB {
+public class MariaDB implements Closeable, AutoCloseable {
 
     private MariaDbPoolDataSource pool;
 
@@ -62,8 +63,48 @@ public class MariaDB {
      * Closes the connection pool.
      * This method should be invoked.
      */
+    @Override
     public void close() {
         pool.close();
+    }
+
+    /**
+     * @param sql the sql statement which should be executed; <br>
+     *            should be a prepared statement
+     * @param parameters the parameters for the prepared statement; <br>
+     *                   at the moment this does only support string, int
+     * @return the result of the sql statement
+     * @throws SQLException if the statement could not be executed
+     */
+    @NotNull
+    public ResultSet executePreparedStatement(String sql, Object... parameters) throws SQLException {
+
+        try (
+                Connection connection = getPool().getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ) {
+
+            int i = 1;
+            for (Object parameter : parameters) {
+
+                if (parameter instanceof String) {
+                    preparedStatement.setString(i, (String) parameter);
+                }
+                else if (parameter instanceof Integer) {
+                    preparedStatement.setInt(i, (int) parameter);
+                }
+                else {
+                    throw new IllegalArgumentException("The executePreparedStatement() method only allows string and int parameters!");
+                }
+
+                i++;
+
+            }
+
+            return preparedStatement.executeQuery();
+
+        }
+
     }
 
     /**
@@ -71,7 +112,7 @@ public class MariaDB {
      * This method won't validate the connection.
      * For that see {@link #validate(Connection, int)}
      */
-    public boolean isConnected(Connection connection) {
+    public static boolean isConnected(Connection connection) {
 
         try {
 
@@ -95,7 +136,7 @@ public class MariaDB {
      *                has time to validate itself
      * @return This will return true if the connection is still open.
      */
-    public boolean validate(Connection connection, int timeout) {
+    public static boolean validate(Connection connection, int timeout) {
 
         try {
 
@@ -108,59 +149,6 @@ public class MariaDB {
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
-        }
-
-    }
-
-    /**
-     * see {@link #executePreparedStatementWithResult(String, boolean, Object...)}
-     */
-    public void executePreparedStatement(String sql, boolean retry, Object... parameters) {
-        executePreparedStatementWithResult(sql, retry, parameters);
-    }
-
-    /**
-     * @param sql the sql statement which should be executed;
-     *            should be a prepared statement
-     * @param parameters the parameters for the prepared statement;
-     *                   at the moment this does only support string, int
-     * @return the result of the sql statement
-     */
-    @Nullable
-    public ResultSet executePreparedStatementWithResult(String sql, boolean retry, Object... parameters) {
-
-        try {
-
-            try (Connection connection = getPool().getConnection()) {
-
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-                int i = 1;
-                for (Object parameter : parameters) {
-                    if (parameter instanceof String) {
-                        preparedStatement.setString(i, (String) parameter);
-                    }
-                    else if (parameter instanceof Integer) {
-                        preparedStatement.setInt(i, (int) parameter);
-                    } else {
-                        throw new IllegalArgumentException("The executePreparedStatement() method only allows string and int parameters!");
-                    }
-                    i++;
-                }
-
-                return preparedStatement.executeQuery();
-
-            }
-
-        } catch (SQLException e) {
-
-            if (retry) {
-                return executePreparedStatementWithResult(sql, false, parameters);
-            } else {
-                e.printStackTrace();
-                return null;
-            }
-
         }
 
     }

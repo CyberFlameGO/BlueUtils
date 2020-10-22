a7de302cf09d0dcefbf8990b70c9a4006f00af11
bluefireoly <bluefireolymp@gmail.com>
2020-10-22 21:30:38 +0200

    Update MongoDB.kt

--- src/main/java/net/axay/blueutils/database/mongodb/MongoDB.kt
+++ src/main/java/net/axay/blueutils/database/mongodb/MongoDB.kt
@@ -6,13 +6,15 @@ import com.mongodb.MongoClientSettings
 import com.mongodb.client.*
 import net.axay.blueutils.database.DatabaseLoginInformation
 import org.bson.Document
+import org.bson.UuidRepresentation
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
 import java.io.Closeable
 
 class MongoDB(
         private val loginInformation: DatabaseLoginInformation,
-        kMongo: Boolean = true
+        kMongo: Boolean = true,
+        spigot: Boolean = false
 ): Closeable {
 
     val mongoClient: MongoClient
@@ -20,11 +22,15 @@ class MongoDB(
 
     init {
 
+        if (spigot)
+            System.setProperty("org.litote.mongo.test.mapping.service", "org.litote.kmongo.jackson.JacksonClassMappingTypeService")
+
         val clientSettings = MongoClientSettings.builder()
             .applyToClusterSettings { builder ->
                 builder.hosts(listOf(loginInformation.mongoServerAddress))
             }
             .credential(loginInformation.mongoCredential)
+            .uuidRepresentation(UuidRepresentation.STANDARD)
         .build()
 
         mongoClient = if (kMongo) KMongo.createClient(clientSettings) else MongoClients.create(clientSettings)

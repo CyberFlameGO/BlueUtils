f7ffad9820b0ac4ea4ffe951b73ba8b26345eddd
bluefireoly <bluefireolymp@gmail.com>
2020-12-03 22:11:38 +0100

    Update MongoExtensions.kt

--- src/main/java/net/axay/blueutils/database/mongodb/MongoExtensions.kt
+++ src/main/java/net/axay/blueutils/database/mongodb/MongoExtensions.kt
@@ -2,7 +2,11 @@ package net.axay.blueutils.database.mongodb
 
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.model.CountOptions
+import org.bson.BsonValue
 import org.bson.conversions.Bson
+import org.litote.kmongo.id.WrappedObjectId
 
-fun <T> MongoCollection<T>.contains(filter: Bson)
-        = countDocuments(filter, CountOptions().limit(1)) == 1L
+fun <T> MongoCollection<T>.contains(filter: Bson) =
+    countDocuments(filter, CountOptions().limit(1)) == 1L
+
+fun <T> BsonValue.asKMongoId() = WrappedObjectId<T>(asObjectId().value)

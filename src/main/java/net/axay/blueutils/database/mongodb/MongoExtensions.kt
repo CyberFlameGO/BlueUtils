77f222fcfae3ea849134e7fb88d8bc4f3d61cb7a
bluefireoly <bluefireolymp@gmail.com>
2020-12-27 21:35:45 +0100

    Added extensions for CoroutineCollection

--- src/main/java/net/axay/blueutils/database/mongodb/MongoExtensions.kt
+++ src/main/java/net/axay/blueutils/database/mongodb/MongoExtensions.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package net.axay.blueutils.database.mongodb
 
 import com.mongodb.ErrorCategory
@@ -7,7 +9,14 @@ import com.mongodb.client.model.CountOptions
 import com.mongodb.client.result.InsertOneResult
 import org.bson.BsonValue
 import org.bson.conversions.Bson
+import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.id.WrappedObjectId
+
+// COMMON
+
+fun <T> BsonValue.asKMongoId() = WrappedObjectId<T>(asObjectId().value)
+
+// SYNC
 
 fun <T> MongoCollection<T>.contains(filter: Bson) =
     countDocuments(filter, CountOptions().limit(1)) == 1L
@@ -19,4 +28,14 @@ fun <T> MongoCollection<T>.insertOneCatchDuplicate(document: T): InsertOneResult
         if (exc.error.category == ErrorCategory.DUPLICATE_KEY) InsertOneResult.unacknowledged() else throw exc
     }
 
-fun <T> BsonValue.asKMongoId() = WrappedObjectId<T>(asObjectId().value)
+// COROUTINE
+
+suspend fun <T : Any> CoroutineCollection<T>.contains(filter: Bson) =
+    countDocuments(filter, CountOptions().limit(1)) == 1L
+
+suspend fun <T : Any> CoroutineCollection<T>.insertOneCatchDuplicate(document: T): InsertOneResult =
+    try {
+        insertOne(document)
+    } catch (exc: MongoWriteException) {
+        if (exc.error.category == ErrorCategory.DUPLICATE_KEY) InsertOneResult.unacknowledged() else throw exc
+    }

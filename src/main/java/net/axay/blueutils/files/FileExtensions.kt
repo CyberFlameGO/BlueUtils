3f19f91162c76c3dd59fdae76403f3c8e18e53bb
bluefireoly <bluefireolymp@gmail.com>
2020-12-27 21:38:37 +0100

    Add documentation

--- src/main/java/net/axay/blueutils/files/FileExtensions.kt
+++ src/main/java/net/axay/blueutils/files/FileExtensions.kt
@@ -2,6 +2,11 @@ package net.axay.blueutils.files
 
 import java.io.File
 
+/**
+ * Creates this file, aswell as all needed parent directories.
+ *
+ * @return true, if a new file was created
+ */
 fun File.createIfNotExists(): Boolean {
     return if (!exists()) {
         if (isDirectory)
